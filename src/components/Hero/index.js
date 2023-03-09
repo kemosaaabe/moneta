44c7b607e1609da93e604c4ec52d6eefa65cad39
kemosaaabe <sanchellos27@mail.ru>
2023-03-09 18:27:23 +0300

    created html layout for hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,19 @@
 const Hero = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div>
+                <h1></h1>
+                <p></p>
+                <div>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </div>
+            <div>
+                <img src="/assets/img/mockup_hero.svg" alt="main mockup" />
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
