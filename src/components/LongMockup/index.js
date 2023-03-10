cf3bffe453178f2252cdd5cb4c133df66d8b8542
kemosaaabe <sanchellos27@mail.ru>
2023-03-11 01:06:57 +0300

    made styles for long mockup section

--- src/components/LongMockup/index.js
+++ src/components/LongMockup/index.js
@@ -5,11 +5,18 @@ const LongMockup = () => {
     return (
         <div className={styles.longMockup}>
             <div className={styles.mockupImg}>
-                <img src="" alt="" />
+                <img src="/assets/img/long_mockup.svg" alt="" />
             </div>
             <div className={styles.mockupText}>
-                <h2></h2>
-                <p></p>
+                <h2>
+                    Наше приложение поможет грамотно и эффективно{' '}
+                    <span>управлять финансами</span>
+                </h2>
+                <p>
+                    Moneta разработана для того, чтобы у вас была полная
+                    финансовая прозрачность и рациональное использование ваших
+                    средств
+                </p>
                 <DownloadButtons />
             </div>
         </div>
