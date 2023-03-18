139417a11e601d35135ba1db3f1db53b4d944367
kemosaaabe <sanchellos27@mail.ru>
2023-03-18 15:16:23 +0300

    [+] html FAQ

--- src/components/FreqAskQuestions/index.js
+++ src/components/FreqAskQuestions/index.js
@@ -1,7 +1,19 @@
 import styles from './FreqAskQuestions.module.scss';
 
 const FreqAskQuestions = () => {
-    return <div>хуй</div>;
+    return (
+        <div className={styles.faqBlock}>
+            <div className={styles.title}></div>
+            <div className={styles.faqContent}>
+                <div className={styles.faqImg}></div>
+                <div className={styles.faqText}>
+                    <p>Доступно ли приложение для IOS?</p>
+                    <p>Доступно ли приложение для IOS?</p>
+                    <p>Сколько категорий можно добавить?</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default FreqAskQuestions;
